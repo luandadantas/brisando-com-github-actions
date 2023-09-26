3106729855839116bccaff2af399cd6ef18001a2
Luanda Dantas <luanda.dantas@jusbrasil.com.br>
2023-09-26 19:50:49 -0300

    conserta falhas encontradas pelo flake8

--- calculadora.py
+++ calculadora.py
@@ -1,11 +1,14 @@
 def soma(a, b):
     return a + b
 
+
 def subtracao(a, b):
     return a - b
 
+
 def multiplicacao(a, b):
     return a * b
+
 
 def divisao(a, b):
     if b == 0:
--- test_calculadora.py
+++ test_calculadora.py
@@ -1,11 +1,13 @@
 import pytest
 from calculadora import soma, subtracao, multiplicacao, divisao
 
+
 def test_soma():
     assert soma(2, 3) == 5
     assert soma(-1, 1) == 0
     assert soma(0, 0) == 0
     assert soma(10, -5) == 5
+
 
 def test_subtracao():
     assert subtracao(5, 3) == 2
@@ -13,11 +15,13 @@ def test_subtracao():
     assert subtracao(-1, 1) == -2
     assert subtracao(0, 0) == 0
 
+
 def test_multiplicacao():
     assert multiplicacao(2, 3) == 6
     assert multiplicacao(-1, 1) == -1
     assert multiplicacao(0, 5) == 0
     assert multiplicacao(4, 0.5) == 2
+
 
 def test_divisao():
     assert divisao(6, 2) == 3

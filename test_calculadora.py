00402369c550fc94111d91b664dd386cbe0f050d
Luanda Dantas <luanda.dantas@jusbrasil.com.br>
2023-09-28 22:54:38 -0300

    sobe teste falhando pra quebrar cd

--- test_calculadora.py
+++ test_calculadora.py
@@ -3,7 +3,7 @@ from calculadora import soma, subtracao, multiplicacao, divisao
 
 
 def test_soma():
-    assert soma(2, 3) == 5
+    assert soma(2, 3) == 4
     assert soma(-1, 1) == 0
     assert soma(0, 0) == 0
     assert soma(10, -5) == 5
